24bbe93f96f9b7121dc4cb58f198594d61f80de3
K. Liutskevich <k.liutskevich@gmail.com>
2018-11-03 00:24:47 +0300

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://de-light.by/admin/');
+define('HTTP_CATALOG', 'http://de-light.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://de-light.by/admin/');
+define('HTTPS_CATALOG', 'http://de-light.by/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vhosts/mebelgood.by/de-light.by/admin/');
+define('DIR_SYSTEM', '/var/www/vhosts/mebelgood.by/de-light.by/system/');
+define('DIR_IMAGE', '/var/www/vhosts/mebelgood.by/de-light.by/image/');
+define('DIR_LANGUAGE', '/var/www/vhosts/mebelgood.by/de-light.by/admin/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/mebelgood.by/de-light.by/admin/view/template/');
+define('DIR_CONFIG', '/var/www/vhosts/mebelgood.by/de-light.by/system/config/');
+define('DIR_CACHE', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/vhosts/mebelgood.by/de-light.by/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mebel_dl');
+define('DB_PASSWORD', '8X0azq1!');
+define('DB_DATABASE', 'mebelav_by_delight');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://de-light.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://de-light.by/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vhosts/mebelgood.by/de-light.by/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/mebelgood.by/de-light.by/system/');
+define('DIR_IMAGE', '/var/www/vhosts/mebelgood.by/de-light.by/image/');
+define('DIR_LANGUAGE', '/var/www/vhosts/mebelgood.by/de-light.by/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/mebelgood.by/de-light.by/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/mebelgood.by/de-light.by/system/config/');
+define('DIR_CACHE', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/download/');
+define('DIR_LOGS', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/vhosts/mebelgood.by/de-light.by/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mebel_dl');
+define('DB_PASSWORD', '8X0azq1!');
+define('DB_DATABASE', 'mebelav_by_delight');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
